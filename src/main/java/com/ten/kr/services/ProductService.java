a5f80d6279709ba25f121cff4e431e8c222f898f
Ten Aleksander <89169604848t@gmail.com>
2022-05-11 22:55:58 +0300

    ver.1

---
+++ src/main/java/com/ten/kr/services/ProductService.java
@@ -0,0 +1,18 @@
+package com.ten.kr.services;
+
+import com.ten.kr.entity.Product;
+import com.ten.kr.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+}
